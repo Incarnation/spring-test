fe7b33232761c6a88293f870b00b229da88f1057
Eric <erichuan2020@gmail.com>
2020-10-18 20:40:31 -0400

    add PhotoAppApiAccountManagement project

--- PhotoAppApiAccountManagement/src/main/java/com/example/photoapp/accountmanagement/PhotoAppApiAccountManagementApplication.java
+++ PhotoAppApiAccountManagement/src/main/java/com/example/photoapp/accountmanagement/PhotoAppApiAccountManagementApplication.java
@@ -1,4 +1,4 @@
-package com.example.PhotoAppApiUser;
+package com.example.photoapp.accountmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PhotoAppApiUserApplication {
+public class PhotoAppApiAccountManagementApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(PhotoAppApiUserApplication.class, args);
+		SpringApplication.run(PhotoAppApiAccountManagementApplication.class, args);
 	}
 
 }
---
+++ PhotoAppApiAccountManagement/src/test/java/com/example/photoapp/accountmanagement/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoapp.accountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
