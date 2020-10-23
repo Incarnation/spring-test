adb02967aa5eb52153efa093283a1c47c50b2e9b
Eric <erichuan2020@gmail.com>
2020-10-22 23:09:02 -0400

    add authentication filter

--- PhotoAppApiUser/src/main/java/com/example/PhotoAppApiUser/security/WebSecurity.java
+++ PhotoAppApiUser/src/main/java/com/example/PhotoAppApiUser/security/WebSecurity.java
@@ -23,6 +23,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //super.configure(auth);
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("**").hasIpAddress(environment.getProperty("gateway.ip"));
+        http.authorizeRequests().antMatchers("**").hasIpAddress(environment.getProperty("gateway.ip"))
+        .and()
+        .addFilter(getAuthenticationFiler());
+        http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFiler() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+        return authenticationFilter;
     }
 }
