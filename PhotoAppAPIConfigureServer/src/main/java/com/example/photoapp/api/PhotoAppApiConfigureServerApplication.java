c6337a7065655b82d9860d8b03b9eec9d088ae2d
Eric <erichuan2020@gmail.com>
2020-10-25 23:11:15 -0400

    add APIConfigureServer

---
+++ PhotoAppAPIConfigureServer/src/main/java/com/example/photoapp/api/PhotoAppApiConfigureServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoapp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiConfigureServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiConfigureServerApplication.class, args);
+	}
+
+}
